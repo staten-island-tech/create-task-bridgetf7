1ad4e67fb1174d1c4b3db457eca89f8a40f78142
bridgetf7 <fbridget0404@gmail.com>
2023-02-09 22:17:04 -0500

    commit

---
+++ create-task/js/DOMSelectors.js
@@ -0,0 +1,11 @@
+const DOMSelectors = {
+    description: document.getElementById("description"),
+    grade: document.getElementById("grade"),
+    weight: document.getElementById("weight"),
+    calculate: document.getElementById("calculate"),
+    clear: document.getElementById("clear"),
+    weightedAvg: document.getElementById("weightedAvg"),
+    letterGrade: document.getElementById("letterGrade"),
+}
+
+export { DOMSelectors };
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,1 +1,35 @@
 import "/styles/style.css";
+import { DOMSelectors } from "./DOMSelectors";
+
+//function used to clear user inputs
+function clear(){
+    DOMSelectors.description.value = "";
+    DOMSelectors.grade.value = "";
+    DOMSelectors.weight.value = "";
+}
+
+//when the clear button is pressed, the user inputs are cleared
+DOMSelectors.clear.addEventListener("click", function () {
+clear();
+});
+
+//takes the user's input for weight and divides it by 100 to obtain percent decimal
+DOMSelectors.calculate.addEventListener("click", function () {
+let weight = DOMSelectors.weight.value;
+let newWeight = weight / 100;
+console.log(newWeight);
+});
+
+const grades=[]; //[] indicates that a new array is created
+const weights=[]; 
+
+
+grades.push(DOMSelectors.grade.value);
+console.log(grades);
+
+
+
+const totalValues = grades.length; //finds out the total number of items in the "grades" array
+console.log(totalValues);
+
+
